02811a259ae95226d44a13fe49336b47d3b6bd8b
Daniil Karlov <dskarlov@icloud.com>
2023-03-19 14:22:00 +0300

    Finish project

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-import os
-
+from flask import request, jsonify
 from flask import Flask
+from marshmallow import ValidationError
+
+from builder import build_query
+from models import RequestSchema
 
 app = Flask(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "data")
 
-
-@app.route("/perform_query")
+@app.route("/perform_query", methods=["POST"])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    data = request.json
+    try:
+        RequestSchema().load(data)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
+
+    first_result = build_query(
+        cmd=data['cmd1'],
+        value=data['value1'],
+        file_name=data['file_name'],
+        data=None,
+    )
+    second_result = build_query(
+        cmd=data['cmd2'],
+        value=data['value2'],
+        file_name=data['file_name'],
+        data=first_result,
+    )
+
+    return jsonify(second_result)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ builder.py
@@ -0,0 +1,33 @@
+import os
+from typing import Callable, Dict
+
+from functions import filter_query, unique_query, limit_query, map_query, sort_query
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+CMS_TO_FUNCTIONS: Dict[str, Callable] = {
+    'filter': filter_query,
+    'unique': unique_query,
+    'limit': limit_query,
+    'map': map_query,
+    'sort': sort_query,
+}
+
+
+def read_file(file_name: str):
+    with open(file_name) as file:
+        for line in file:
+            yield line
+
+
+def build_query(cmd, value, file_name, data):
+    if data is None:
+        prepared_data = read_file(DATA_DIR + os.sep + file_name)
+    else:
+        prepared_data = data
+
+    func = CMS_TO_FUNCTIONS[cmd]
+    func_result = func(value=value, data=prepared_data)
+
+    return list(func_result)
